eb24c413976432dfce1141169375f0e3a1d45e95
Chumankin Yuriy <chumankinyuriy@mail.ru>
2016-09-11 02:05:38 +0400

    Доработан шаблон проекта по разработке библиотеки с нейронными сетями

--- ANNLib/ANN.cpp
+++ ANNLib/ANN.cpp
@@ -1,7 +1,113 @@
+#define ANNDLL_EXPORTS
 #include <ANN.h>
+#include <fstream>
+#include <iomanip>
 
-int ANN::func()
+
+bool ANN::NeuralNetwork::Read(std::string filepath)
 {
-	return 13;
+	std::ifstream file(filepath);
+	if (!file.is_open()) return false;
+	int buffer;
+	const int CHAR_BUF_LEN = 100;
+	char char_buffer[CHAR_BUF_LEN];
+	file.getline(char_buffer, CHAR_BUF_LEN);
+	std::string string_buffer = std::string(char_buffer);
+	memset(char_buffer, 0, CHAR_BUF_LEN);
+	if (string_buffer != std::string("activation type:")) 
+		throw "incorrect file format";
+	file >> buffer;
+	activation_type = (ActivationType)buffer;
+	file.getline(char_buffer, CHAR_BUF_LEN);
+	file.getline(char_buffer, CHAR_BUF_LEN);
+	string_buffer = std::string(char_buffer);
+	memset(char_buffer, 0, CHAR_BUF_LEN);
+	if (string_buffer != std::string("activation scale:"))
+		throw "incorrect file format";
+	file >> scale;
+	file.getline(char_buffer, CHAR_BUF_LEN);
+	file.getline(char_buffer, CHAR_BUF_LEN);
+	string_buffer = std::string(char_buffer);
+	memset(char_buffer, 0, CHAR_BUF_LEN);
+	if (string_buffer != std::string("configuration:")) 
+		throw "incorrect file format";
+	file >> buffer;
+	configuration.resize(buffer);
+	for (size_t i = 0; i < configuration.size(); i++) {
+		file >> configuration[i];
+	}
+	file.getline(char_buffer, CHAR_BUF_LEN);
+	file.getline(char_buffer, CHAR_BUF_LEN);
+	string_buffer = std::string(char_buffer);
+	memset(char_buffer, 0, CHAR_BUF_LEN);
+	if (string_buffer != std::string("weights:"))
+		throw "incorrect file format";
+	weights.resize(configuration.size()-1);
+	for (size_t i = 1; i < weights.size(); i++) {
+		weights[i].resize(configuration[i]);
+		for (size_t j = 0; j < weights.size(); j++) {
+			weights[i][j].resize(configuration[i-1]);
+			for (size_t k = 0; k < weights[i][j].size(); k++) {
+				file >> weights[i][j][k];
+			}
+		}
+	}
+	file.close();
+	is_trained = true;
+	return true;
 }
 
+bool ANN::NeuralNetwork::Save(std::string filepath)
+{
+	if (!is_trained) return false;
+	std::ofstream file(filepath);
+	if (!file.is_open()) return false;
+	file << std::setprecision(9);
+	file << "activation type:" << std::endl;
+	file << (int)activation_type << std::endl;
+	file << "activation scale:" << std::endl;
+	file << scale << std::endl;
+	file << "configuration:" << std::endl;
+	file << configuration.size() << "\t";
+	for each (int neuron_count in configuration) {
+		file << neuron_count << "\t";
+	}
+	file << std::endl << "weights:" << std::endl;
+	for each (auto weight_matrix in weights) {
+		for each (auto weight_line in weight_matrix) {
+			for each (auto weight in weight_line) {
+				file << weight << " ";
+			}
+			file << std::endl;
+		}
+	}
+	file.close();
+	return true;
+}
+
+std::vector<int> ANN::NeuralNetwork::GetConfiguration()
+{
+	return configuration;
+}
+
+float ANN::NeuralNetwork::Activation(float neuronInput)
+{
+	if (activation_type == POSITIVE_SYGMOID) {
+		return (1.f / (1.f + expf(-scale * neuronInput)));
+	}
+	else if (activation_type == BIPOLAR_SYGMOID) {
+		return (2.f / (1.f + expf(-scale * neuronInput)) - 1.f);
+	}
+	return -1.f;
+}
+
+float ANN::NeuralNetwork::ActivationDerivative(float activation)
+{
+	if (activation_type == POSITIVE_SYGMOID) {
+		return scale * activation * (1.f - activation);
+	}
+	else if (activation_type == BIPOLAR_SYGMOID) {
+		return scale * 0.5f * (1.f + activation) * (1.f - activation);
+	}
+	return -1;
+}
--- ANNLib/ANN.h
+++ ANNLib/ANN.h
@@ -1,12 +1,73 @@
 #pragma once
 
 #ifdef ANNDLL_EXPORTS
-#define ANNDDLL_API __declspec(dllexport) 
+#define ANNDLL_API __declspec(dllexport) 
 #else
-#define ANNDSDLL_API __declspec(dllimport) 
+#define ANNDLL_API __declspec(dllimport) 
 #endif
+
+#include <vector>
+#include <memory>
 
 namespace ANN
 {
-	ANNDSDLL_API int func();
+	class NeuralNetwork
+	{
+	public:
+		enum ActivationType
+		{
+			POSITIVE_SYGMOID,
+			BIPOLAR_SYGMOID
+		};
+		/**Прочитать нейронную сеть из файла
+		 *@param
+		 */
+		virtual bool Read(std::string filepath);
+		virtual bool Save(std::string filepath);
+		std::vector<int> GetConfiguration();
+
+		/**********************Это вам надо реализовать самим**********************/
+		virtual std::string GetType() = 0;
+		virtual std::vector<float> Predict(std::vector<float> & input) = 0;
+		virtual float MakeTrain(
+			std::vector<std::vector<float>> & inputs,
+			std::vector<std::vector<float>> & outputs,
+			int max_iters = 10000,
+			float eps = 0.1,
+			float speed = 0.1
+		) = 0;
+
+	protected:
+		/**Веса сети*/
+		std::vector<std::vector<std::vector<float> > > weights;
+		/**Конфигурация сети.
+		 * номер элемета в массиве соответсвует номеру слоя
+		 * значение - количеству нейронов
+		 */
+		std::vector<int> configuration;
+		/**Обучена ли сеть?*/
+		bool is_trained;
+		/**Масштабирующий коэффициент аргумента сигмоиды*/
+		float scale;
+		/**Тип активационной функции*/
+		ActivationType activation_type;
+		/**Вычислить значение активационной функции
+		 *@param neuron_input - входное значение нейрона
+		 *@return - значение активационной фунции
+		 */
+		float Activation(float neuron_input);
+		/**Вычислить значение производной активационной функции
+		 *@param activation - значение активационной фнункции, для которой хотим вычислить производную
+		 *@return - значение производной активационной фунции
+		 */
+		float ActivationDerivative(float activation);
+	};
+
+	/**Создать нейронную сеть
+	 *@param configuration - конфигурация нейронной сети
+	 */
+	ANNDLL_API std::shared_ptr<ANN::NeuralNetwork> CreateNeuralNetwork(
+		std::vector<int> & configuration = std::vector<int>(),
+		NeuralNetwork::ActivationType activation_type = NeuralNetwork::POSITIVE_SYGMOID
+	);
 }
